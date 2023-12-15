4b490bc0c865c764da2a917968de4e2da8c807d8
sfcg2 <castillo.sofia@correounivalle.edu.co>
2023-12-15 11:15:48 -0500

    version 1 queue

--- Taller-Practico-main/queue/src/Colas.java
+++ Taller-Practico-main/queue/src/Colas.java
@@ -4,47 +4,68 @@ import java.util.Scanner;
 
 public class Colas {
 
-    private int numero;
+    private int numero = 1;
+    Queue<String> fila = new LinkedList<String>();
 
 
     public static void main(String[] args) throws Exception {
 
-        Scanner scanner = new Scanner(System.in);
-        
-        Queue<Integer> fila = new LinkedList<Integer>();
-        Colas colas = new Colas();
-       
-        
-        System.out.println("holi");
+        try (Scanner scanner = new Scanner(System.in)) {
+            Colas colas = new Colas();
+      
+            
+            System.out.println("holi");
 
-        System.out.print("Ingrese un numero: ");
+            System.out.print("Ingrese un numero: ");
+            int n = scanner.nextInt();
+            
+            System.out.println("funcion");
 
-        colas.setNumero(scanner.nextInt()); 
-        
-        System.out.println("funcion");
-        colas.enqueue(colas);
+            colas.enqueue(n);
+        }
     }
     
     String cadena = "";
 
-    public void enqueue(Colas cola){
+    public void enqueue(int n){
 
-        
+        int i = 1;
 
-        int actualizarnum = (cola.getNumero() / 2);
-        cadena += String.valueOf(cola.getNumero() % 2);
-        System.out.println("residuo" + actualizarnum);
+        while(i <= n ){
+            System.out.println("i = " + i );
+            int actualizarnum = (numero / 2); // 0
+            cadena += String.valueOf(numero % 2); //1 
+            System.out.println("division " + actualizarnum); //0
+            System.out.println("modulo " + (1%2));
+            System.out.println("cadena " + cadena); //1
 
-        System.out.println("ggg " + cadena);
-        numero = actualizarnum;
-        System.out.println(numero);
+            //numero = actualizarnum;
+            setNumero(actualizarnum); ////0
+            System.out.println("nuevo numero " + getNumero()+ "\n"); 
 
-
+            if(actualizarnum == 0 ){
+                fila.add(cadena);
+                cadena = "";
+                i++;
+                setNumero(i);
+                
+            }
+        }System.out.println(fila);
+        System.out.println(fila.peek());
+        System.out.println(fila.poll());
+        System.out.println(fila.peek());
     }
+
+    
+
+
+
+
+
+
     public int getNumero() {
         return numero;
     }
-
 
     public void setNumero(int numero) {
         this.numero = numero;
